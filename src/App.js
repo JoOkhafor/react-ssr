dc377591cdf30d981ea6a9ccc320055b2389ece0
Jo O'khafor <jookhafor@gmail.com>
2023-11-17 16:39:05 +0000

    second-commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
